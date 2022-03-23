28ee0844be111bca90597229d69ba4eaf7dbdbc9
nbrennan26 <nbrennan26@gmail.com>
2022-03-23 15:06:41 -0500

    early app.js logic added. User model created

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const express = require("express")
 const path = require("path")
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+
+// dotenv setup
+require("dotenv").config()
+const PORT = process.env.PORT || 3000
+
+// mongoDB & mongoose setup
+const mongoDB = process.env.MONGO_DB
+mongoose.connect(mongoDB, {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+})
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "MongoDB connection Error:"))
+
+// View Engine setup
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  username: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false, // This will not include password when pulling a users info. Manually pull it with ".select('+password')"
+  },
+  member_statue: {
+    type: Boolean,
+  },
+});
+
+// Virtual for User's Full Name
+UserSchema.virtual("fullname").get(function () {
+  let fullname = this.first_name + " " + this.last_name;
+  return fullname;
+});
+
+// Virtual for User's URL
+UserSchema.virtual("url").get(function () {
+  return "/user/" + this._id;
+});
+
+// Export Model
+module.exports = mongoose.model("User", UserSchema);
